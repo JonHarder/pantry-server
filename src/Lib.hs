f3b919914c1c45578a8beac0fc4c8be1ff0e2b8c
Jon Harder <jonharder6@gmail.com>
2020-02-04 22:04:10 -0600

    import scotty for webserver

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,8 +4,14 @@ module Lib
     ( runApp
     ) where
 
+
+import Data.Monoid (mconcat)
 import Data.Text.IO as IO
+import Web.Scotty
 
 
 runApp :: IO ()
-runApp = IO.putStrLn "running app.."
+runApp = scotty 8000 $
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
