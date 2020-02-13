71308cb60ddfba261e2332ecd87ac3483b357fe0
Jon Harder <jonharder6@gmail.com>
2020-02-13 12:15:26 -0600

    get all or search by name

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,20 +23,15 @@ import System.Environment
 import System.IO
 
 
--- type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
---       :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
---       :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
---       :<|> "err" :> Get '[JSON] ()
-
-
-type API = "recipes" :> Get '[JSON] [Recipe]
+type API = "recipes" :> QueryParam "name" String :> Get '[JSON] [Recipe]
       :<|> "recipes" :> Capture "recipeId" Int :> Get '[JSON] (Maybe Recipe)
       :<|> "recipes" :> ReqBody '[JSON] Recipe :> Post '[JSON] RecipePostResponse
+      :<|> "err"     :> Get '[JSON] ()
 
 
 data RecipePostResponse
-  = Sucess { recipeId :: Int }
-  | Failure { error :: String }
+  = RecipePostSuccess { recipeId :: Int }
+  | RecipePostFailure { error :: String }
   deriving Generic
 
 instance ToJSON RecipePostResponse
@@ -52,30 +47,51 @@ instance ToJSON Recipe
 instance FromJSON Recipe
 
 
--- server :: Server API
--- server = position
---     :<|> hello
---     :<|> marketing
---     :<|> (throwError $ err404 { errBody = "error: error not found" })
+keyLime :: Recipe
+keyLime = Recipe "Key Lime Pie" ["limes"] ["smash the limes"]
+
+
+bread :: Recipe
+bread = Recipe "Sourdough Bread" ["flour", "water", "salt"] ["it's complicated...."]
+
+
+recipes :: [Recipe]
+recipes = [keyLime, bread]
+
+
+getRecipes :: Maybe String -> Handler [Recipe]
+getRecipes mName =
+  case mName of
+    Just name | name == "pie" -> return [keyLime]
+              | name == "bread" -> return [bread]
+              | otherwise -> return []
+    Nothing ->
+      return recipes
+
         
+getRecipe :: Int -> Handler (Maybe Recipe)
+getRecipe recipeId = return $
+  if recipeId == 2 then
+    Just keyLime
+  else
+    Nothing
+
+
+postRecipe :: Recipe -> Handler RecipePostResponse
+postRecipe r = do
+  liftIO $ putStrLn $ "saving recipe: " ++ name r
+  return $
+    if name r == "foo" then
+      RecipePostFailure "that name doesn't make any sense"
+    else
+      RecipePostSuccess 8
+
 
 server :: Server API
-server = recipes
-    :<|> recipe
-    :<|> recipePost
-  where recipes = do
-          liftIO $ putStrLn "getting all the recipes"
-          return [keyLime]
-        recipe recipeId = return $
-          if recipeId == 2 then
-            Just keyLime
-          else
-            Nothing
-        recipePost recipe = do
-          liftIO $ putStrLn $ "saving recipe: " ++ name recipe
-          return $ Sucess 1
-        keyLime = Recipe "Key Lime Pie" ["limes"] ["smash the limes"]
-
+server = getRecipes
+    :<|> getRecipe
+    :<|> postRecipe
+    :<|> (throwError $ err500 { errBody = "I just wanted to see what would happen"})
 
 api :: Proxy API
 api = Proxy
